5af25e1019c46ebaf2f50e80703554e03a07eb4c
HaroldWalter <harold11walter@gmail.com>
2021-03-20 01:06:48 +0100

    formulaire

---
+++ public_html/vues/pages/mentions-legales.php
@@ -0,0 +1,3 @@
+<main>
+<h1>Mentions Légales</h1>
+</main>
---
+++ public_html/vues/pages/prise-de-rdv.php
@@ -0,0 +1,35 @@
+<main>
+<form action="" method="post">
+   <div>
+   <label for="nom">Nom : </label><br>
+   <input type="text" id="nom" name="nom" required>
+   </div>
+   <div>
+   <label for="prenom">Prénom : </label><br>
+   <input type="text" id="prenom" name="prenom" required>
+   </div>
+   <div>
+   <fieldset>
+   <legend>Est-ce votre premier rendez-vous ?</legend>
+   <br>
+   <input type="radio" name="premierRdv" id="oui" value="oui">
+   <label for="oui">Oui</label><br>
+   <input type="radio" name="premierRdv" id="non" value="non">
+   <label for="non">Non</label>
+   </fieldset>
+   </div>
+   <div>
+   <label for=""></label>
+   <input type="text">
+   </div>
+   <div>
+   <label for="numeroTel">N° de Téléphone : </label><br>
+   <input type="tel" id="numeroTel" name="numeroTel" required>
+   </div>
+   <div>
+   <label for="numeroSecu">N° de Sécurité Sociale : </label><br>
+   <input type="number" id="numeroSecu" name="numeroSecu" size="15" required>
+   </div>
+
+</form>
+</main>
